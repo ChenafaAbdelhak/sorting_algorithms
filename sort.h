e225ca786500372f916afd82aa12b75827a21014
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2024-01-23 16:30:31 -0800

    task 1

--- sort.h
+++ sort.h
@@ -22,5 +22,11 @@ typedef struct listint_s
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
+void swap_node(listint_t **list, listint_t *node1, listint_t *node2);
+void insertion_sort_list(listint_t **list);
+int partition(int *array, int lo, int hi, size_t size);
+void quicksort(int *array, int lo, int hi, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
